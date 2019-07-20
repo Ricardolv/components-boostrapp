296c55841f563c649d563f658e4fa6133d90835b
Ricardolv <richardluizv@gmail.com>
2019-07-20 11:54:48 -0300

    add Pagination Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { RectiveFormComponent } from './rective-form/rective-form.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TemplateDriveFormComponent } from './template-drive-form/template-drive-form.component';
+
 import { MustMatchDirective } from './_helpers';
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { RectiveFormComponent } from './rective-form/rective-form.component';
+import { TemplateDriveFormComponent } from './template-drive-form/template-drive-form.component';
+import { JwPaginationComponent } from 'jw-angular-pagination';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RectiveFormComponent,
     TemplateDriveFormComponent,
+    JwPaginationComponent,
+    PaginationComponent,
+
     MustMatchDirective
   ],
   imports: [
---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css']
+})
+export class PaginationComponent implements OnInit {
+  items = [];
+  pageOfItems: Array<any>;
+
+  constructor() { }
+
+  ngOnInit() {
+    // an example array of 150 items to be paged
+    this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}`}));
+  }
+
+  onChangePage(pageOfItems: Array<any>) {
+    // update current page of items
+    this.pageOfItems = pageOfItems;
+  }
+
+}
